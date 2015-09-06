fb75a3e2f532f1a947e769d457f68cf5c718a569
KarinnaLoo <karinnaloo@pacbell.net>
2015-09-06 00:54:06 -0400

    working code, no database

---
+++ app/src/main/java/com/pennapps/cashcat/ShowTransactions.java
@@ -0,0 +1,42 @@
+package com.pennapps.cashcat;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by kloo on 9/5/15.
+ */
+public class ShowTransactions extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_transactions);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayUseLogoEnabled(false);
+        getActionBar().setIcon(android.R.color.transparent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.findItem(R.id.transactions).setVisible(false);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
